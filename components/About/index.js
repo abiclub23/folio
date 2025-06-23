5140ca8d6e46d4ee05c885ecef1102c4e7ebca87
Abhi Tondepu <atondepu@gmail.com>
2025-06-22 17:02:18 -0700

    daabout update

--- components/About/index.js
+++ components/About/index.js
@@ -15,14 +15,16 @@ export default function About() {
           <section>
             <p className="text-lg leading-relaxed text-gray-700">
             I'm a dad, engineer, and Manchester United fan who enjoys writing in my spare time. This site is where I share my thoughts and musings.
-            For me finishing and sharing a post matters more than perfecting it, so please excuse the writing quality at times. 
-            
+            For me finishing and sharing a post matters more than perfecting it, so please excuse the occasional rough edge in my writing. 
+            <br />
+            <br />
+            This site started as a good idea in my head, a place for me to capture and share whatever’s on my mind. The voice in my head insists that some of this will be valuable to someone, somewhere. So here you go: my little corner of the internet.
             </p>
           </section>
 
           <section className="mt-12 pt-6 border-t border-gray-950">
             <p className="text-lg text-gray-700">
-            Thanks for visiting! Feel free to reach out if you want to say
+            Thanks for stopping by, and I hope you find something here that resonates with you. Feel free to reach out if you want to say
               <a href="mailto:atondepu@gmail.com" className="text-blue-600 hover:underline ml-1">
                 hello
               </a>.
