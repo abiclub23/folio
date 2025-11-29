2c4fd6596e7ac0c29746a1066534344614708545
Abhi Tondepu <abiclub@gmail.com>
2025-11-28 22:05:10 -0800

    thought updates

--- data/thoughts.js
+++ data/thoughts.js
@@ -6,20 +6,42 @@ export const thoughts = [
   {
     text: "Been into fonts lately.",
     timestamp: "April 14, 2025"
-  }, {
-    text: "My 2 year old fell down and yelled at the floor.",
-    timestamp: "August 15, 2025"
   },
   {
-    text: "Been into fonts lately.",
-    timestamp: "April 14, 2025"
-  },{
-    text: "My 2 year old fell down and yelled at the floor.",
-    timestamp: "August 15, 2025"
+    text: "I miss driving my blue pulsar.",
+    timestamp: "June 10, 2025"
   },
   {
-    text: "Been into fonts lately.",
-    timestamp: "April 14, 2025"
+    text: "I'm noticing a lack of beauty in city buildings… the city doesn't care about delight.",
+    timestamp: "November 11, 2025"
+  },
+  {
+    text: "I was doing some learning with AI… using it that way for the first time and seeing the benefits was amazing.",
+    timestamp: "November 16, 2025"
+  },
+  {
+    text: "Parenting is a difficult job. Sometimes I wish we had someone to pat us on the back and say \"good job, it's difficult.\"",
+    timestamp: "November 18, 2025"
+  },
+  {
+    text: "Need to stop looking for shortcuts and just do the actual work.",
+    timestamp: "November 18, 2025"
+  },
+  {
+    text: "This year went by extremely fast. The whole year felt like a month. Where did it all go? Damn!",
+    timestamp: "November 18, 2025"
+  },
+  {
+    text: "Cicero — \"If you have a garden and a library, you have everything you need.\"",
+    timestamp: "November 21, 2025"
+  },
+  {
+    text: "\"The best type of risks to take are ones where (1) the worst outcome is manageable and (2) the best outcome is life-changing.\"",
+    timestamp: "November 21, 2025"
+  },
+  {
+    text: "It's a privilege to spend time with kids when they are young.",
+    timestamp: "November 23, 2025"
   }
 ];
 
